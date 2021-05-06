cac5faf4a0ab6bbfad323de9653aba77a587c002
Maksim Trigolos <Maksim_Trigolos@epam.com>
2021-05-07 01:42:50 +0400

    feat: add nginx

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 import { PingController } from './controllers/ping/ping.controller';
 import { QuotesController } from './controllers/quotes/quotes.controller';
 import { QuotesService } from './services/quotes/quotes.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client'),
-      exclude: ['/api*', '/ping'],
-    }),
-  ],
+  imports: [ConfigModule.forRoot()],
   controllers: [PingController, QuotesController],
   providers: [QuotesService],
 })
